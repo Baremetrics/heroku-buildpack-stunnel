c2ffadf21b9597af486ba63bb420d5091561eecb
OhadArzouan <ohad@xenon.io>
2021-07-05 21:58:13 +0300

    add db_name to connection string

--- bin/legacy-stunnel-conf.sh
+++ bin/legacy-stunnel-conf.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-URLS=${REDIS_STUNNEL_URLS:-REDIS_URL `compgen -v HEROKU_REDIS`}
+URLS=${STUNNEL_URLS}
 n=1
 
 mkdir -p /app/vendor/stunnel/var/run/stunnel/
@@ -9,11 +9,13 @@ foreground = yes
 
 pid = /app/vendor/stunnel/stunnel4.pid
 
+sslVersion = all
 options = NO_SSLv2
+options = NO_SSLv3
+fips=no
 options = SINGLE_ECDH_USE
 options = SINGLE_DH_USE
 socket = r:TCP_NODELAY=1
-options = NO_SSLv3
 TIMEOUTidle = 86400
 ciphers = HIGH:!ADH:!AECDH:!LOW:!EXP:!MD5:!3DES:!SRP:!PSK:@STRENGTH
 debug = ${STUNNEL_LOGLEVEL:-notice}
@@ -22,13 +24,14 @@ EOFEOF
 for URL in $URLS
 do
   eval URL_VALUE=\$$URL
-  PARTS=$(echo $URL_VALUE | perl -lne 'print "$1 $2 $3 $4 $5 $6 $7" if /^([^:]+):\/\/([^:]+):([^@]+)@(.*?):(.*?)(\/(.*?)(\\?.*))?$/')
+  PARTS=$(echo $URL_VALUE | perl -lne 'print "$1 $2 $3 $4 $5 $6 $7" if /^([^:]+):\/\/([^:]+):([^@]+)@(.*?):(.*?)\/([^?]+)\?(.*)$/')
   if [ -z "$PARTS" ]
   then
-    PARTS=$(echo $URL_VALUE | perl -lne 'print "$1 $2 $3 $4 $5 $6 $7" if /^([^:]+):\/\/:([^@]+)@(.*?):(.*?)(\/(.*?)(\\?.*))?$/')
+    PARTS=$(echo $URL_VALUE | perl -lne 'print "$1 $2 $3 $4 $5 $6 $7" if /^([^:]+):\/\/:([^@]+)@(.*?):(.*?)\/([^?]+)\?(.*)$/')
     WITHOUT_USERNAME=true
   fi
   URI=( $PARTS )
+
 
   if [ "$WITHOUT_USERNAME" = true ] ; then
     URI_SCHEME=${URI[0]}
@@ -36,9 +39,10 @@ do
     URI_HOST=${URI[2]}
     URI_PORT=${URI[3]}
     STUNNEL_PORT=$((URI_PORT + 1))
+    URI_DB_NAME=${URI[4]
 
     echo "Setting ${URL}_STUNNEL config var"
-    export ${URL}_STUNNEL=$URI_SCHEME://:$URI_PASS@127.0.0.1:637${n}
+    export ${URL}_STUNNEL=$URI_SCHEME://:$URI_PASS@127.0.0.1:4342${n}/$URI_DB_NAME
   else
     URI_SCHEME=${URI[0]}
     URI_USER=${URI[1]}
@@ -46,15 +50,16 @@ do
     URI_HOST=${URI[3]}
     URI_PORT=${URI[4]}
     STUNNEL_PORT=$((URI_PORT + 1))
+    URI_DB_NAME=${URI[5]}
 
     echo "Setting ${URL}_STUNNEL config var"
-    export ${URL}_STUNNEL=$URI_SCHEME://$URI_USER:$URI_PASS@127.0.0.1:637${n}
+    export ${URL}_STUNNEL=$URI_SCHEME://$URI_USER:$URI_PASS@127.0.0.1:4342${n}/$URI_DB_NAME
   fi
 
   cat >> /app/vendor/stunnel/stunnel.conf << EOFEOF
 [$URL]
 client = yes
-accept = 127.0.0.1:637${n}
+accept = 127.0.0.1:4342${n}
 connect = $URI_HOST:$STUNNEL_PORT
 retry = ${STUNNEL_CONNECTION_RETRY:-"no"}
 EOFEOF
