b28e4d2760739a9516e042e43455e27e2ad05842
OhadArzouan <ohad@xenon.io>
2021-07-06 10:49:34 +0300

    small bug fixes

--- bin/stunnel-conf.sh
+++ bin/stunnel-conf.sh
@@ -15,7 +15,6 @@ TIMEOUTidle = 86400
 ciphers = HIGH:!ADH:!AECDH:!LOW:!EXP:!MD5:!3DES:!SRP:!PSK:@STRENGTH
 debug = ${STUNNEL_LOGLEVEL:-notice}
 EOFEOF
-# debug
 for URL in $URLS
 do
   eval URL_VALUE=\$$URL
@@ -33,31 +32,34 @@ do
     URI_PASS=${URI[1]}
     URI_HOST=${URI[2]}
     URI_PORT=${URI[3]}
-    STUNNEL_PORT=$((URI_PORT + 1))
+    STUNNEL_PORT=$URI_PORT
     URI_DB_NAME=${URI[4]}
+    QUERY_PARAMS=${URI[5]}
 
-    echo "Setting ${URL}_STUNNEL config var"
-    export ${URL}_STUNNEL=$URI_SCHEME://:$URI_PASS@127.0.0.1:4342${n}/$URI_DB_NAME
+    echo "Setting ${URL} config var"
+    export $URL=$URI_SCHEME://:$URI_PASS@127.0.0.1:4342${n}/$URI_DB_NAME?$QUERY_PARAMS
+
   else
     URI_SCHEME=${URI[0]}
     URI_USER=${URI[1]}
     URI_PASS=${URI[2]}
     URI_HOST=${URI[3]}
     URI_PORT=${URI[4]}
-    STUNNEL_PORT=$((URI_PORT + 1))
+    STUNNEL_PORT=$URI_PORT
     URI_DB_NAME=${URI[5]}
+    QUERY_PARAMS=${URI[6]}
 
-    echo "Setting ${URL}_STUNNEL config var"
-    export ${URL}_STUNNEL=$URI_SCHEME://$URI_USER:$URI_PASS@127.0.0.1:4342${n}/$URI_DB_NAME
+    echo "Setting ${URL} config var"
+    export $URL=$URI_SCHEME://$URI_USER:$URI_PASS@127.0.0.1:4342${n}/$URI_DB_NAME?$QUERY_PARAMS
   fi
 
   cat >> /app/vendor/stunnel/stunnel.conf << EOFEOF
-[$URL]
-client = yes
-accept = 127.0.0.1:4342${n}
-connect = $URI_HOST:$STUNNEL_PORT
-retry = ${STUNNEL_CONNECTION_RETRY:-"no"}
-EOFEOF
+  [$URL]
+  client = yes
+  accept = 127.0.0.1:4342${n}
+  connect = $URI_HOST:$STUNNEL_PORT
+  retry = ${STUNNEL_CONNECTION_RETRY:-"no"}
+  EOFEOF
 
   let "n += 1"
 done
